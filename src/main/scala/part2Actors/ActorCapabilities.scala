b137d4747e272c549254a3370442bd71978612cd
Žan Žibert <zzibert@plume.com>
2021-11-23 22:52:05 +0100

    10. Actors, Messages and Behaviors - Exercises

--- src/main/scala/part2Actors/ActorCapabilities.scala
+++ src/main/scala/part2Actors/ActorCapabilities.scala
@@ -1,5 +1,6 @@
 package part2Actors
 
+import akka.actor.Status.Success
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ActorCapabilities extends App {
@@ -57,6 +58,98 @@ object ActorCapabilities extends App {
 
   case class WirelessPhoneMessage(content: String, ref: ActorRef)
 
-  alice ! WirelessPhoneMessage("Hi!", ref = bob)
+//  alice ! WirelessPhoneMessage("Hi!", ref = bob)
+
+  // 1. create a counter actor
+  // internal int counter
+  // increment, decrement, print messages
+  case class Increment(amount: Int)
+  case class Decrement(amount: Int)
+  case object Print
+
+  class Counter extends Actor {
+    var counter = 0
+    override def receive: Receive = {
+      case Increment(amount) => counter += amount
+      case Decrement(amount) => counter -= amount
+      case Print => println(s"[Counter Actor] the current count is $counter")
+    }
+  }
+
+  val counterActor = system.actorOf(Props[Counter], "counter")
+
+  counterActor ! Increment(125)
+
+  counterActor ! Decrement(100)
+
+  counterActor ! Print
+
+  // 2. Bank acc as an Actor
+  /*
+  *  Deposit an amount, replying with Success, Failure messages
+  * Withdraw an amount
+  * Statement
+  * interact with some other kind of actor
+  * */
+
+  case class Deposit(id: Int, amount: Int)
+  case class Withdraw(id: Int, amount: Int)
+  case object Statement
+  case class Success(id: Int)
+  case class Failure(id: Int)
+
+  class Client(ref: ActorRef) extends Actor {
+    override def receive: Receive = {
+      case Deposit(id, amount) => ref ! Deposit(id, amount)
+      case Withdraw(id, amount) => ref ! Withdraw(id, amount)
+      case Success(id) => println(s"[Client Actor] The transaction: $id was Successfull")
+      case Failure(id) => println(s"[Client Actor] The transaction: $id Failed")
+      case Statement => ref ! Statement
+      case amount: Int => println(s"[Client Actor] The amount on the account is $amount")
+    }
+  }
+
+  class Bank extends Actor {
+    var account = 0
+    override def receive: Receive = {
+      case Deposit(id, amount) => {
+        if (amount < 0) {
+          sender() ! Failure(id)
+        } else {
+          account += amount
+          sender() ! Success(id)
+        }
+      }
+      case Withdraw(id, amount) => {
+        if (amount > account) {
+          sender() ! Failure(id)
+        } else {
+          account -= amount
+          sender() ! Success(id)
+        }
+      }
+      case Statement => sender() ! account
+    }
+  }
+
+  val bank = system.actorOf(Props[Bank], "bankActor")
+
+  val clientProps = Props(new Client(bank))
+
+  val client = system.actorOf(clientProps, "clientActor")
+
+  client ! Deposit(1, 1000)
+
+  client ! Deposit(2, 2000)
+
+  client ! Withdraw(3, 1000)
+
+  client ! Statement
+
+  client ! Withdraw(4, 1000)
+
+  client ! Withdraw(5, 1000)
+
+  client ! Withdraw(6, 1000)
 
 }
